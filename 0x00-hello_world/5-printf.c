f7bfc44106ebb654044685bd43e03bb5cf1e01d0
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-09-07 11:20:00 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Do not use puts
+ * compile with warning when using -wall gcc
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
