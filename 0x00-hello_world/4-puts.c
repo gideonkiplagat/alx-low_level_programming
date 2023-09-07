9d53eb144b0542a82fe38b304770f16d468e4d41
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-09-07 11:06:54 +0300

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Not allowed to use printf
+ * use the function puts
+ * program should end with value 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
