be7f62661649018640d3f8ee884b241f69ff983f
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-09-12 15:24:06 +0300

    print fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    long int fib1 = 1, fib2 = 2, next;
+    int count;
+
+    printf("%ld, %ld, ", fib1, fib2);
+
+    for (count = 3; count <= 50; count++)
+    {
+        next = fib1 + fib2;
+        printf("%ld", next);
+
+        if (count < 50)
+            printf(", ");
+        else
+            printf("\n");
+
+        fib1 = fib2;
+        fib2 = next;
+    }
+
+    return (0);
+}
