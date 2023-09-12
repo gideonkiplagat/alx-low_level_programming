39873c8c2be0c0e9777ebc95ed8ca27d1d9bae34
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-09-12 17:26:03 +0300

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Tests the positive_or_negative() function with 0.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    i = 0;
+    positive_or_negative(i);
+
+    return (0);
+}
