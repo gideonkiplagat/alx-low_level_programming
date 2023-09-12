f6e057a0cc1e94d7fea507621a0212e638b7d5c2
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-09-12 14:56:58 +0300

    prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,29 +5,37 @@
  */
 void times_table(void)
 {
-	int i, j, result;
+	int x, y, z, u, d;
 
-	for (i = 0; i <= 9; i++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (y = 0; y <= 9; y++)
 		{
-			result = i * j;
+			z = x * y;
 
-			if (j == 0)
+			if (z > 9)
 			{
-				_putchar('0' + result);
+				u = z % 10;
+				d = (z - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
 
-				if (result <= 9)
-					_putchar(' ');
-
-				_putchar('0' + result);
+				_putchar(z + '0');
 			}
 		}
+
 		_putchar('\n');
 	}
 }
