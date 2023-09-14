a191970d66940ffa63467375fcbe6aaef3a7cba5
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-09-14 14:14:23 +0300

    print a triagle using #

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,24 +3,32 @@
 /**
  * print_triangle - Prints a right-angled triangle in the terminal.
  * @size: The size of the triangle.
+ *
+ * Return: void
  */
 void print_triangle(int size)
 {
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        int i, j;
+	int x, y, z;
 
-        for (i = 1; i <= size; i++)
-        {
-            for (j = 1; j <= i; j++)
-            {
-                _putchar(35); // ASCII representation of '#'
-            }
-            _putchar('\n');
-        }
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
+			}
+
+			for (z = 0; z <= x; z++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
+		}
+	}
 }
