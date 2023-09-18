ff047d9c5b3af11dd1efa9be555503baf1ecec77
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-09-18 21:35:07 +0300

    Password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,27 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 
 /**
-* main - generates random passwords for 101-crackme
+ * main - Generates random passwords for 101-crackme
+ *
  * Return: Always 0 (Success)
  **/
 int main(void)
 {
-	   int sum;
-	   char c;
-srand(time(NULL));
-	   sum = 0;
-           while (sum <= 2645)
-	   {
-		   c = rand() % 128;
-		   sum += c;
-		   putchar(c);
-	   }
-	   putchar(2772 -sum);
+    int sum;
+    char c;
+    srand(time(NULL));
+    sum = 0;
 
-	return (0);
+    while (sum <= 2645)
+    {
+        c = rand() % 128;
+        sum += c;
+        putchar(c);
+    }
+
+    putchar(2772 - sum);
+
+    return (0);
 }
