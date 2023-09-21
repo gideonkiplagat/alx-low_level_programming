b48f06cf32a8d92a0a0dd028afa1aeb93a401b4e
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-09-21 12:11:24 +0300

    adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,82 +1,69 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * infinite_add - Add two numbers stored as char arrays.
- * @n1: The first number.
- * @n2: The second number.
- * @r: Pointer to the buffer to store the result.
- * @size_r: The size of the buffer.
+ * infinite_add - Add up two numbers stored in given char arrays
+ * @n1: The first number
+ * @n2: The second number
+ * @r: Pointer to the buffer to store result
+ * @size_r: The size of the buffer
  *
- * Return: 0 regarding the condition.
+ * Return: 0 if buffer too small to store result, else return pointer to buffer
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, temp_len, result_len, i, digit_sum, num1, num2, carry;
-	char temp[10000];
+	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+	char tmp[10000];
 
-	result_len = i = len1 = len2 = digit_sum = num1 = num2 = carry = 0;
-
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
-
-	if (len1 + 2 > size_r || len2 + 2 > size_r)
+	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+	while (n1[l1] != '\0')
+		l1++;
+	while (n2[l2] != '\0')
+		l2++;
+	if (l1 + 2 > size_r || l2 + 2 > size_r)
 		return (0);
-
-	len1--;
-	len2--;
-
-	while (i <= len1 || i <= len2)
+	l1--;
+	l2--;
+	while (i <= l1 || i <= l2)
 	{
 		num1 = num2 = 0;
-
-		if (i <= len1)
-			num1 = n1[len1 - i] - '0';
-		if (i <= len2 && (len2 - i) >= 0)
-			num2 = n2[len2 - i] - '0';
-
-		digit_sum = num1 + num2 + carry;
-
-		if (digit_sum >= 10)
+		if (i <= l1)
+			num1 = n1[l1 - i] - '0';
+		if (i <= l2 && (l2 - i) >= 0)
+			num2 = n2[l2 - i] - '0';
+		sum = num1 + num2 + carry;
+		if (sum >= 10)
 		{
 			carry = 1;
-			digit_sum -= 10;
+			sum -= 10;
 		}
 		else
 			carry = 0;
-
-		r[i] = digit_sum + '0';
+		r[i] = sum + '0';
 		i++;
-		result_len++;
+		rl++;
 	}
-
 	if (carry > 0)
 	{
 		r[i] = carry + '0';
 		r[i + 1] = '\0';
 	}
-
-	i = temp_len = 0;
-
-	while (i <= result_len)
+	i = tmpl = 0;
+	while (i <= rl)
 	{
-		temp[i] = r[result_len - i];
-		temp_len++;
+		tmp[i] = r[rl - i];
+		tmpl++;
 		i++;
 	}
-
 	i = 0;
-
-	while (i < temp_len)
+	while (i < tmpl)
 	{
 		if (r[i] == '\0')
 		{
 			break;
 		}
-		r[i] = temp[i];
+		r[i] = tmp[i];
 		i++;
 	}
-
 	return (r);
 }
