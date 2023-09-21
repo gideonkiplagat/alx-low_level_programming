da94885b5b91b6aebf4801aef082da60259f33d6
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-09-21 11:28:12 +0300

    function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *rot13(char *str)
+{
+    int index = 0;
+
+    while (str[index])
+    {
+        while ((str[index] >= 'a' && str[index] <= 'z') || (str[index] >= 'A' && str[index] <= 'Z'))
+        {
+            if ((str[index] > 'm' && str[index] <= 'z') || (str[index] > 'M' && str[index] <= 'Z'))
+            {
+                str[index] -= 13;
+                break;
+            }
+
+            str[index] += 13;
+            break;
+        }
+
+        index++;
+    }
+
+    return (str);
+}
