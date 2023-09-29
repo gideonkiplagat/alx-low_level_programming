3f89ee5dd5ed662be6da8887ffc3e17b302f75b0
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-09-29 13:16:44 +0300

    updated _putchar

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,21 +1,12 @@
 #include "main.h"
 
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
-int main(void)
+ * _putchar - Writes a character to stdout.
+ * @c: The character to print.
+ *
+ * Return: On success.
+ */
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+    return write(1, &c, 1);
 }
