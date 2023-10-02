ca0d1ba756923c8818207b3d683766d70ae96303
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-10-02 13:03:36 +0300

    splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,7 +1,27 @@
 #include <stdlib.h>
-#include "main.h"
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * count_words - Counts the number of words in a string.
+ * @str: The input string.
+ *
+ * Return: The number of words in the string.
+ */
+static int count_words(char *str)
+{
+	int count = 0, i = 0;
+
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+			count++;
+		i++;
+	}
+
+	return (count);
+}
 
 /**
  * strtow - Splits a string into words.
@@ -11,22 +31,43 @@
  */
 char **strtow(char *str)
 {
-	char *array = NULL;
-	unsigned int i = 0, j = 0, k;
+	if (str == NULL || *str == '\0')
+		return (NULL);
 
-	if (strncmp(str, "", 1) || str == NULL)
+	int i = 0, j = 0, k, word_count = count_words(str);
+	char **words;
+
+	words = (char **)malloc((word_count + 1) * sizeof(char *));
+	if (words == NULL)
 		return (NULL);
-	array = malloc((i + j + 1) * sizeof(char));
-	if (array == NULL)
-		return (NULL);
-	for (k = 0; k < i; k++)
-		array[k] = str[k];
-	i = k;
-	for (k = 0; k < j; k++)
+
+	while (str[i])
 	{
-		array[i] = str[k];
-		i++;
+		if (str[i] != ' ')
+		{
+			k = 0;
+			while (str[i] && str[i] != ' ')
+			{
+				i++;
+				k++;
+			}
+			words[j] = (char *)malloc((k + 1) * sizeof(char));
+			if (words[j] == NULL)
+			{
+				for (j = 0; j <= word_count; j++)
+					free(words[j]);
+				free(words);
+				return (NULL);
+			}
+			for (k = 0; str[i - k - 1] != ' ' && k < j; k++)
+				words[j][k] = str[i - k - 1];
+			words[j][k] = '\0';
+			j++;
+		}
+		else
+			i++;
 	}
-	array[i] = '\0';
-	return (NULL);
+	words[j] = NULL;
+	return (words);
 }
+
