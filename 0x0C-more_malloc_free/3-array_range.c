5de8f6ae0ba3bf4853de4fd89d58430be19ae945
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-10-05 12:50:34 +0300

    creates an array of integers.

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,28 +2,33 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * array_range - Create array of integers from min - max.
- * @min: The minimum value.
- * @max: The maximum value.
- *
- * Return: Pointer to new array, NULL if failed
- */
+  * _abs - get absolute value
+  * @n: number to calculate
+  * Return: absolute value of the number
+  */
+int _abs(int n)
+{
+	n < 0 ? (n *= -1) : (n = n);
+	return (n);
+}
+/**
+  * array_range - creates an array of integers
+  * @min: The minimum value
+  * @max: The maximum value
+  * Return: pointer to array of integers or NULL.
+  */
 int *array_range(int min, int max)
 {
-	int *arr;
-	int i, size, n;
+	int *ptr;
+	int range, i;
 
 	if (min > max)
 		return (NULL);
-	size = max - min;
-	arr = malloc((size + 1) * sizeof(int));
-	i = 0;
-	n = min;
-	while (n <= max)
-	{
-		arr[i] = n;
-		i++;
-		n++;
-	}
-	return (arr);
+	range = max - min + 1;
+	ptr = malloc(range * sizeof(*ptr));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < range; min++, i++)
+		ptr[i] = min;
+	return (ptr);
 }
