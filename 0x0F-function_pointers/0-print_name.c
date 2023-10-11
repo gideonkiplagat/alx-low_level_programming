06660ee9a811c5ea422d4572cacb5d8b9753cb76
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-10-11 13:00:39 +0300

    unction that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "func_pointers.h"
+
+/**
+  * print_name - Prints a name using func pointer.
+  * @name: The name to be printed.
+  * @f: Pointer to the printing function.
+  *
+  * Return: Void.
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
