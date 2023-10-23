70d837c8613ed22cbc05682a02928d41b87e1bbf
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-10-23 13:16:08 +0300

    frees a listint_t list, with double pointers

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list and sets the head to NULL.
+ * @head: A pointer to a pointer to the head of the list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head)
+	{
+		while (*head)
+		{
+			current = (*head);
+			*head = (*head)->next;
+			free(current);
+		}
+	}
+	else
+	{
+		return;
+	}
+
+	free(*head);
+	head = 0;
+}
