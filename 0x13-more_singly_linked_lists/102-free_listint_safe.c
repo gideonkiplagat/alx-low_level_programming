0ac50aee483f5f141a4d078dbb922512ece2811c
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-10-24 16:38:49 +0300

    frees a listint_t list.

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,52 +1,30 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t list, even with a loop.
- * @h: A pointer to a pointer to the head of the list.
+ * free_listint_safe - prints a list
+ * @h: address of pointer to first node
  *
- * Return: The size of the list that was freed.
+ * Return: address of head
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *killnode;
-	listint_t *current;
-	listadd_t *headadd;
-	listadd_t *checker;
-	size_t count;
+	size_t c = 0;
+	listint_t *head, *temp;
+	long diff;
 
-	count = 0;
-	current = *h;
-	headadd = NULL;
-	if (h != NULL)
+	if (!h)
+		return (0);
+	head = *h;
+	*h = NULL;
+	while (head)
 	{
-		while (current != NULL)
-		{
-			checker = headadd;
-			while (checker != NULL)
-			{
-				if (current == checker->address)
-				{
-					free(current);
-					free_listadd(headadd);
-					/*headadd = NULL;*/
-					 *h = NULL;
-					return (count);
-				}
-				checker = checker->next;
-			}
-			killnode = current;
-			if (add_nodeaddress(&headadd, current) == NULL)
-			{
-				free_listadd(headadd);
-				exit(98);
-			}
-			current = current->next;
-			free(killnode);
-			count++;
-		}
-		free_listadd(headadd);
-		*h = NULL;
+		c++;
+		diff = head->next - head;
+		temp = head;
+		free(temp);
+		if (diff >= 0)
+			break;
+		head = head->next;
 	}
-	return (count);
+	return (c);
 }
