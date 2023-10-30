c3841da5454de64bb237e40449298c484cbbf98e
gideonkiplagat <kiplagatgideon00@gmail.com>
2023-10-30 13:06:09 +0300

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
-include "main.h"
+#include "main.h"
 
 /**
- * create_file - Creates a file with specified permission.
+ * create_file - Creates a file with specified permissions.
  * @filename: The name of the file to create or truncate
  * @text_content: The text content to write to the file.
  *
@@ -9,22 +9,34 @@ include "main.h"
  */
 int create_file(const char *filename, char *text_content)
 {
-	int new_file, len, wr_stat;
+	int fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (new_file == -1)
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-	{
-		close(new_file);
-		return (1);
-	}
-	for (len = 0; text_content[len]; len++)
-		;
-	wr_stat = write(new_file, text_content, len);
-	if (close(new_file) == -1)
-		return (-1);
-	return (wr_stat == -1 ? -1 : 1);
+
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+	return (1);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }
